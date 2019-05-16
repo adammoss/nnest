ba9c312452b2d6151c6008c4962cb02a5f107900
Adam Moss <adam.moss@nottingham.ac.uk>
2019-05-16 23:25:34 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "nnest",
-    version = "0.1.1",
+    version = "0.1.2",
     description = "Neural network nested sampling",
     author = "Adam Moss",
     author_email = "adam.moss@nottingham.ac.uk",
